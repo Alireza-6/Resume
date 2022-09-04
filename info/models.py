e0a222a4f0606621029431af2214c796e409656c
Alireza-6 <areza.mirzayi@gmail.com>
2022-09-04 23:40:12 +0430

    Some Models Created

--- info/models.py
+++ info/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
-# Create your models here.
+
+def image_upload_path():
+    result = 'profile'
+    return result
+
+
+class SiteSetting(models.Model):
+    copyright_text = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.copyright_text}'
+
+
+class PersonalInfo(models.Model):
+    name = models.CharField(max_length=50)
+    job_title = models.CharField(max_length=50)
+    about_me = models.TextField()
+    address = models.CharField(max_length=200)
+    email = models.EmailField(max_length=50)
+    profile_image = models.ImageField(upload_to=image_upload_path())
+    phone = models.CharField(max_length=11)
+    linkedin = models.CharField(max_length=20)
+    github = models.CharField(max_length=20)
+    is_active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Badge(models.Model):
+    name = models.CharField(max_length=25)
+    description = models.CharField(max_length=50, blank=True, null=True)
+    description_link = models.URLField(blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
+
+
+class Skill(Badge):
+    order = models.SmallIntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Familiarity(Badge):
+    pass
+
+    def __str__(self):
+        return f'{self.name}'
